eee70a3b8cb7d4f8c28e790ae83bdc3c2f47bb6e
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-02-16 23:12:58 -0800

    update: I don't want to use external libraries as much as possible

--- serverselecter.py
+++ serverselecter.py
@@ -1,58 +1,49 @@
 import requests
 import typer
-import serverjars as sj
+import json
+from loguru import logger
+from sys import stderr
+import asyncio
 
-class ReleaseStates:
-  STABLE = 'stable'
-  EXPERIMENTAL = 'experimental'
-  UNSTABLE = 'unstable'
-  TESTING = 'testing'
-  SNAPSHOT = 'snapshot'
-  
-class ServerTypes:
-  BEDROCK='bedrock'
-  MODDED='modded'
-  PROXIES='proxies'
-  SERVERS='servers'
+def debug_init(trace, debug):
+    logger.remove()
+    if debug:
+        logger.add(stderr, level='DEBUG')
+    elif trace:
+        logger.add(stderr, level='TRACE')
+    else:
+        logger.add(stderr, level='INFO')
+        pass
+    pass
 
-class JarTypes:
-  NUKKITX='nukkitx'
-  POCKETMINE='pocketmine'
-  MOHIST='mohist'
-  FORGE='forge'
-  CATSERVER='catserver'
-  FABRIC='fabric'
-  BUNGEECORD='bungeecord'
-  VELOCITY='velocity'
-  WATERFALL='waterfall'
-  FLAMECORD='flamecord'
-  BUKKIT='bukkit'
-  PAPER='paper'
-  SPIGOT='spigot'
-  PURPUR='purpur'
-  TUINITY='tuinity'
-  SPONGE='sponge'
-  SNAPSHOT='snapshot'
-  VANILLA='vanilla'
-
-
+debug_init(True, False)
 
 class ServerJars:
-  def __init__(self, software:ServerTypes, jartype):
-    self.jartype = jartype
-    self.software = software
+  def __init__(self):
+    self._apie='https://serverjars.com/api'#Api Endpoint
+  
+  async def queue_data(self):
+    getrequest=f'{self._apie}/fetchTypes'
+    logger.trace(getrequest)
+    JarTypeList = requests.api.get(getrequest)
+    parsed = json.loads(JarTypeList.content)
+    response=parsed['response']
+    dumped = json.dumps(response, indent=4)
+    self.fetchTypes=dumped
     
-    
-  def list_types(self):
-    '''Returns a list with all available types'''
-    types = sj.fetchTypes()
-    return types
-  def list_jars(self, software):
-    jars = sj.fetchAll(software)
-    
+  async def jar_types(self):
+    return self.fetchTypes
 
 
-soft=software()
-jars=ServerJars(software.servers)
-print(jars.list_types())
-    
+
+async def main():
+  jars = ServerJars()
+  await jars.queue_data()
+  input('Fetch Jar Types? ')
+  
+  print(await jars.jar_types())
+  
+
+
+if __name__ == "__main__":
+  asyncio.run(main())
