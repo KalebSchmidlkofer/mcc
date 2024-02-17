49186b2700ed1ff659791c0bf23ff7c64a4ee07e
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-02-17 00:29:07 -0800

    update: lots of changes/fixes

--- serverselecter.py
+++ serverselecter.py
@@ -1,9 +1,11 @@
 import requests
-import typer
 import json
 from loguru import logger
 from sys import stderr
 import asyncio
+import cProfile
+from time import sleep
+import time
 
 def debug_init(trace, debug):
     logger.remove()
@@ -13,37 +15,44 @@ def debug_init(trace, debug):
         logger.add(stderr, level='TRACE')
     else:
         logger.add(stderr, level='INFO')
-        pass
-    pass
-
-debug_init(True, False)
 
 class ServerJars:
   def __init__(self):
-    self._apie='https://serverjars.com/api'#Api Endpoint
-  
-  async def queue_data(self):
-    getrequest=f'{self._apie}/fetchTypes'
+    self._apie='https://serverjars.com/api'
+
+  async def queue_types(self):
+    start=time.time()
+    getrequest = f'{self._apie}/fetchTypes'
     logger.trace(getrequest)
-    JarTypeList = requests.api.get(getrequest)
-    parsed = json.loads(JarTypeList.content)
-    response=parsed['response']
-    dumped = json.dumps(response, indent=4)
-    self.fetchTypes=dumped
-    
+    response = await self.fetch(getrequest)
+    self.fetchTypes = response['response']
+    end=time.time()
+    logger.debug(f'Queue_types time: {end-start}')
+
+  async def fetch(self, url):
+    response = await asyncio.to_thread(requests.get, url)
+    return response.json()
+
   async def jar_types(self):
     return self.fetchTypes
 
+  async def jar_details(self, jartype, category, version=None):
+    if version is None:
+      getrequest = f'fetchDetails/{jartype}/{category}'
+    else:
+      getrequest = f'fetchDetails/{jartype}/{category}/{version}'
 
-
-async def main():
-  jars = ServerJars()
-  await jars.queue_data()
-  input('Fetch Jar Types? ')
-  
-  print(await jars.jar_types())
-  
+debug_init(True, False)
+async def main(jars:ServerJars):
+  print(jars.fetchTypes)
+  category = input('Category: ')
+  version = input('Mc Version (Leave Blank for latest): ')
+  # print(await jars.jar_types())
+  print('done')
 
 
 if __name__ == "__main__":
-  asyncio.run(main())
+  jars = ServerJars()
+  asyncio.run(jars.queue_types())
+  asyncio.run(main(jars))
+  
